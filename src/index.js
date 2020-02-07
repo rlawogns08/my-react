d756b3a9a28bd9da9db54dc1f66075ac3521cb6c
jhkim <rlawogns08@gmail.com>
2020-02-07 13:22:09 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+class App extends ReactComponent {
+    render() {
+        return (
+            <h3>안녕하세요 리액트입니다</h3>
+        );
+    }
+}
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
