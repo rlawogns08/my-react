87c8af187eb13c5e7d7ea7eedcd940e90079d8e8
jhkim <rlawogns08@gmail.com>
2020-02-07 16:39:51 +0900

    기본적인 카운트 내부 state

--- src/index.js
+++ src/index.js
@@ -2,17 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-class App extends ReactComponent {
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0,
+        };
+    }
+
+    countUp() {
+        this.setState({
+            count: this.state.count + 1,
+        });
+    }
+
+    countDown() {
+        this.setState({
+            count: this.state.count - 1,
+        });
+    }
+
     render() {
         return (
-            <h3>안녕하세요 리액트입니다</h3>
+            <div className="app">
+                <h3>안녕하세요 리액트입니다 Redux 실험</h3>
+                <input type="button" value="버튼" onClick={()=>{alert("클릭")}} />
+                {/* <input type="button" value="숫자업" onClick={()=>{countUp()}} /> */}
+                {/* <input type="button" value="숫자다운" onClick={()=>{countDown()}} /> */}
+                <h3>카운트를 세보아요</h3>
+                <input type="button" value="숫자업" onClick={() => {this.countUp()}} />
+                <input type="button" value="숫자다운" onClick={()=> this.countDown() } />
+                <hr/>
+                {this.state.count}
+            </div>
         );
     }
 }
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+);
 
 
 
